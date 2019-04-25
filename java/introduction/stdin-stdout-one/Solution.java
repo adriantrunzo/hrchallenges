259a91c24c0a40aa5b050fb65c9817f8e4574993
Adrian Trunzo <adriantrunzo@users.noreply.github.com>
2019-04-24 19:42:26 -0600

    Add solutions for java proficiency.

---
+++ java/introduction/stdin-stdout-one/Solution.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner inputReader = new Scanner(System.in);
+
+        for (int i = 0; i < 3; i++) {
+            int num = inputReader.nextInt();
+            System.out.println(num);
+        }
+
+        inputReader.close();
+    }
+}
